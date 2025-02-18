6a7f333fe717430d543b9d3d5e3636b4b4d46b93
Siddharth Chowdhury <chyani341@gmail.com>
2025-02-17 23:59:56 -0700

    mid commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Array;
+
+public class Main{
+    public static ArrayList<Integer> allLessThanMean(ArrayList<Integer> list){
+        
+
+    }
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        
+    }
+}
