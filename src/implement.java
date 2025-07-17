7d6638cb7e721f2b9765ae5caae833d23eb44ba9
Siddharh A. Chowdhury <siddharth@Siddharhs-MacBook-Air.local>
2025-07-17 20:07:42 +0600

    queues LL

--- src/implement.java
+++ src/implement.java
@@ -1,9 +1,8 @@
-import pkg.LinkedList;
-import pkg.StackAL.Stack;
+import pkg.QueuesLL.Queue;
 
 public class implement {
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
+        /*LinkedList list = new LinkedList();
         list.addFirst("a");
         list.addFirst("is");
         list.addLast("Linked List");
@@ -14,6 +13,7 @@ public class implement {
         list.printList();
         System.out.println(list.getSize());
 
+        
         Stack stack = new Stack();
         stack.push(30);
         stack.push(20);
@@ -24,6 +24,20 @@ public class implement {
             System.out.println(stack.pop());
         }
         
-        System.out.println(stack.peek());
+        System.out.println(stack.peek());*/
+
+
+        Queue queue = new Queue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+
+        while(!queue.isEmpty()){
+            System.out.println(queue.dequeue());
+        }
+        System.out.println(queue.peek());
+
+        
+
     }
 }
