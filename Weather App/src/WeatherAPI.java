e7f26d4eea4a4306be818d265f02a651f54bc7d0
Siddharth-Chy1967 <schowd64@asu.edu>
2025-07-26 20:16:54 +0600

    nun

---
+++ Weather App/src/WeatherAPI.java
@@ -0,0 +1,13 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+//https://open-meteo.com/en/docs/geocoding-api?count=1&name=New+York
+public class WeatherAPI{
+    
+}
